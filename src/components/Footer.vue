7b4c0eecafaacc630e78397acce4ea1e15a792cb
IlonaBaran <82875048+IlonaBaran@users.noreply.github.com>
2023-03-27 14:26:46 +0200

    element footer ok

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,65 +1,39 @@
 <template>
     <div id="footer" class="footer">
-        <Button label="Plan du site" icon="pi pi-external-link" @click="visible = true" />
-        <Dialog v-model:visible="visible" modal header="Header" :style="{ width: '50vw' }">
-            <p>
-            <!--
-                <header>
-                <h2 id="titre">Plan du site</h2>
-                </header>
-                <section>
-                    <h4>Nom du site -TO DO</h4>
-                    <ul>
-                        <li>Accueil</li>
-                    </ul>
-                    <ul>
-                        <li>Crédits</li>//
-                        <li>Mentions légales</li>//
-                        <li>Plan du site</li>
-                    </ul>
-                </section>
-                -->
-                blalalaal
-            </p>
+        <Button label="Plan du site" icon="pi pi-external-link" @click="plan = true" />
+        <Dialog v-model:visible="plan" modal header="Plan du site" :style="{ width: '50vw' }">
+              <section>
+                  <h4>Nom du site -TO DO</h4>
+                  <ul><li>Accueil</li></ul>
+                  <ul>
+                      <li>Crédits</li>
+                      <li>Mentions légales</li>
+                      <li>Plan du site</li>
+                  </ul>
+              </section>
         </Dialog>
 
 
-        <Button label="Mentions légales" icon="pi pi-external-link" @click="visible = true" />
-        <Dialog v-model:visible="visible" modal header="Header" :style="{ width: '50vw' }">
-            <p>
-            <!--
-                <header>
-                    <h2 id="titre">Mentions légales</h2>
-                </header>
-                <h4>AUTEUR:</h4>
-                    Ce site a été réalisé par 		Ilona BARAN, Léa FREDOC, Fernando RICO-QUINTERO et Jacqueline WILLIAMS, étudiants en cycle ingénieur ou Master 
-                    'Technologies des Systèmes d'Information' à l'Ecole Nationale des Sciences Géographiques - Géomatique (ENSG - Géomatique) à Marne-la-Vallée.
-                <h4>EDITEUR:</h4>
-                    Ce site est la propriété de l'ENSG - Géomatique. Ce site est édité par l'ENSG - Géomatique, au 6 et 8 Avenue Blaise Pascal, Cité Descartes à Champs-sur-Marne (77455 Marne la Vallée Cedex 2). Cette structure est une composante de l’Institut Géographique National (IGN) qui est un organisme public au statut juridique d’Etablissement Public à caractère Administratif (EPA).
-                <h4>FORME JURIDIQUE:</h4>
-                    Etablissement public à caractère scientifique, culturel et professionnel.
-                <h4>DONNEES PERSONNELLES:</h4>
-                    L’ENSG - Géomatique ne collecte et stocke sur les visiteurs du site aucune autre information nominative ou personnelle que celles qui lui sont ouvertement et volontairement fournies en particulier par l’utilisation des formulaires d’envoi de courriels.
-            -->
-            bébou
-            </p>
+        <Button label="Mentions légales" icon="pi pi-external-link" @click="mentions = true" />
+        <Dialog v-model:visible="mentions" modal header="Mentions légales" :style="{ width: '50vw' }">
+            <h4>AUTEUR:</h4>
+                Ce site a été réalisé par 		Ilona BARAN, Léa FREDOC, Fernando RICO-QUINTERO et Jacqueline WILLIAMS, étudiants en cycle ingénieur ou Master 
+                'Technologies des Systèmes d'Information' à l'Ecole Nationale des Sciences Géographiques - Géomatique (ENSG - Géomatique) à Marne-la-Vallée.
+            <h4>EDITEUR:</h4>
+                Ce site est la propriété de l'ENSG - Géomatique. Ce site est édité par l'ENSG - Géomatique, au 6 et 8 Avenue Blaise Pascal, Cité Descartes à Champs-sur-Marne (77455 Marne la Vallée Cedex 2). Cette structure est une composante de l’Institut Géographique National (IGN) qui est un organisme public au statut juridique d’Etablissement Public à caractère Administratif (EPA).
+            <h4>FORME JURIDIQUE:</h4>
+                Etablissement public à caractère scientifique, culturel et professionnel.
+            <h4>DONNEES PERSONNELLES:</h4>
+                L’ENSG - Géomatique ne collecte et stocke sur les visiteurs du site aucune autre information nominative ou personnelle que celles qui lui sont ouvertement et volontairement fournies en particulier par l’utilisation des formulaires d’envoi de courriels.
         </Dialog>
 
 
-        <Button label="Crédits" icon="pi pi-external-link" @click="visible = true" />
-        <Dialog v-model:visible="visible" modal header="Header" :style="{ width: '50vw'}">
-            <p>
-            <!--
-                <header>
-                    <h2 id="titre">Crédits</h2>
-                </header>
-                <h4>CREDITS DEVELOPPEMENT:</h4>
-                    Le développement de <a href="TO DO" target="_blank"> ce site </a> a été réalisé par Ilona BARAN, Léa FREDOC, Fernando RICO-QUINTERO et Jacqueline WILLIAMS, étudiants en cycle ingénieur ou Master 'Technologies des Systèmes d'Information' à l'Ecole Nationale des Sciences Géographiques - Géomatique à Marne-la-Vallée.                <h4>CREDITS CARTE:</h4>
-                    Leaflet
-                    Site: <a href="https://leafletjs.com/" target="_blank"> https://leafletjs.com/ </a>
-            -->
-            bibiche
-            </p>
+        <Button label="Crédits" icon="pi pi-external-link" @click="credits = true" />
+        <Dialog v-model:visible="credits" modal header="Crédits" :style="{ width: '50vw'}">
+          <h4>CREDITS DEVELOPPEMENT:</h4>
+              Le développement de <a href="TO DO" target="_blank"> ce site </a> a été réalisé par Ilona BARAN, Léa FREDOC, Fernando RICO-QUINTERO et Jacqueline WILLIAMS, étudiants en cycle ingénieur ou Master 'Technologies des Systèmes d'Information' à l'Ecole Nationale des Sciences Géographiques - Géomatique à Marne-la-Vallée.                <h4>CREDITS CARTE:</h4>
+              Leaflet
+              Site: <a href="https://leafletjs.com/" target="_blank"> https://leafletjs.com/ </a>
         </Dialog>
 
 
@@ -78,7 +52,9 @@
     },
     data() {
         return {
-            visible: false
+            plan: false,
+            mentions: false,
+            credits: false
         };
     }
    }
