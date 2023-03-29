35b4c50814b4cd7a3ae72cd9cdfbc4431ecc1f23
Jackigeo <99828439+Jackigeo@users.noreply.github.com>
2023-03-29 10:30:25 +0200

    access to tilt in globe

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,20 +1,38 @@
 <template>
   <div id="viewerDiv" class="viewer">
   </div>
+  <Toolbar :view=view @updateTilt="updateTilt"></Toolbar>
 </template>
 
 <script>
 /* eslint-disable */
 import MapViewer from "../services/MapViewer.js"
+import Toolbar from "./Toolbar.vue";
 
 
 export default {
   name: 'mapComponent',
+  components: {
+    Toolbar,
+  },
+  data() {
+    return {
+      view: null
+    };
+  },
   mounted() {
     const viewerDiv = document.getElementById('viewerDiv');
     const coord = [-3.35291, 47.69651];
-    let mapView = new MapViewer(viewerDiv, coord)
+    let mapView = new MapViewer(viewerDiv, coord);
+    mapView.view.controls.setTilt(10)
+
+
   },
+  methods: {
+    updateTilt(tilt) {
+      mapView.view.controls.setTilt(10);
+    }
+  }
 }
 </script>
 
