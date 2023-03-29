a4d4e1d35851d08b4fda86871b6de81664477975
fernando <fernando.rico.95@outlook.com>
2023-03-29 16:31:23 +0200

    buildings

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -24,9 +24,16 @@ export default {
   },
   created() {
     const viewerDiv = document.getElementById('viewerDiv');
+
   },
   mounted() {
     const coord = [-3.35291, 47.69651];
+
+    //defining projection coordinate unit
+    proj4.defs(
+      'EPSG:2154',
+      '+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
+    );
 
     var placement = {
       coord: new Coordinates('EPSG:4326', coord[0], coord[1]),
@@ -37,24 +44,49 @@ export default {
 
     // ADD NAVIGATION TOOLS :
     new Navigation(view, {
-                position: 'bottom-right',
-                translate: { y: -40 },
-            });
-    
-            var orthoSource = new WMTSSource({
-            url: 'https://wxs.ign.fr/essentiels/geoportail/wmts/',
-            crs: "EPSG:3857",
-            name: 'ORTHOIMAGERY.ORTHOPHOTOS',
-            tileMatrixSet: 'PM',
-            format: 'image/jpeg',
-            style: 'normal'
-        })
+      position: 'bottom-right',
+      translate: { y: -40 },
+    });
 
-        var orthoLayer = new ColorLayer('Ortho', {
-            source: orthoSource,
-        });
+    var orthoSource = new WMTSSource({
+      url: 'https://wxs.ign.fr/essentiels/geoportail/wmts/',
+      crs: "EPSG:3857",
+      name: 'ORTHOIMAGERY.ORTHOPHOTOS',
+      tileMatrixSet: 'PM',
+      format: 'image/jpeg',
+      style: 'normal'
+    })
 
-        view.addLayer(orthoLayer);
+    var orthoLayer = new ColorLayer('Ortho', {
+      source: orthoSource,
+    });
+
+    view.addLayer(orthoLayer);
+
+    const batsource = new FileSource({
+      url: 'gavres_bati.geojson',
+      crs: 'EPSG:2154',
+      format: 'application/json',
+    });
+
+
+    let basic = new FeatureGeometryLayer('basic', {
+      // Use a FileSource to load a single file once
+      source: batsource,
+      transparent: true,
+      opacity: 0.7,
+      //zoom: { min: 10 },
+      style: new Style({
+        fill: {
+          color: new THREE.Color(0xffaaaa),
+          base_altitude: 0,
+          extrusion_height: 40,
+        }
+      })
+    });
+
+    view.addLayer(basic);
+
   },
   methods: {
   }
