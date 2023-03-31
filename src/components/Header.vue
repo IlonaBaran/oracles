9004a013e0e43001811560b1f613d6249228936c
lfredoc <leafredoc@gmail.com>
2023-03-31 12:35:53 +0200

    fix fini

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,35 @@
 
 <template>
-  <div class="toolBar">
-  <Toolbar >
-    <template #start>
+    <div class="toolBar">
+        <Toolbar>
+            <template #start>
 
-        <Button  v-styleclass="{ selector: '.card', toggleClass: 'p-hidden' }" type="button" icon="pi pi-bars" label="" />
-        <MultiSelect v-model="selectedScenario" :options="scenario" filter optionLabel="name" placeholder="Sélection Scénarios"  
-                    :maxSelectedLabels="3" class="w-full md:w-20rem selectScenario margin-right:15px"
-                    :selectedScenario="this.selectedScenario" />
-        <CascadeSelect v-model="selectedGraph" :options="countries" optionLabel="name" optionGroupLabel="name"
-                :optionGroupChildren="['states']" style="min-width: 14rem " placeholder="Sélection Graphique" class="selectGraph" 
-                />
+                <Button v-styleclass="{ selector: '.card', toggleClass: 'p-hidden' }" type="button" icon="pi pi-bars"
+                    label="" />
+                <MultiSelect v-model="selectedScenario" :options="scenario" filter optionLabel="name"
+                    placeholder="Sélection Scénarios" :maxSelectedLabels="3"
+                    class="w-full md:w-20rem selectScenario margin-right:15px" :selectedScenario="this.selectedScenario" />
+                <CascadeSelect v-model="selectedGraph" :options="countries" optionLabel="name" optionGroupLabel="name"
+                    :optionGroupChildren="['states']" style="min-width: 14rem " placeholder="Sélection Graphique"
+                    class="selectGraph" />
 
-<!-- Pour le moment, j'ai besoin de cliquer sur le bouton pour afficher tous les scenarios dans le multiselect -->
-        <button @click="getDossier">Affichage des scenarios</button>
+                <!-- Pour le moment, j'ai besoin de cliquer sur le bouton pour afficher tous les scenarios dans le multiselect -->
+                <button @click="getDossier">Affichage des scenarios</button>
 
-<!-- Il sert a transmettre les paramètres de l'utilisateur a la vue 'Panel': les scenarios choisis et le graph choisis -->
-        <button @click="emitData">Transmettre des données</button>
+                <!-- Il sert a transmettre les paramètres de l'utilisateur a la vue 'Panel': les scenarios choisis et le graph choisis -->
+                <button @click="emitData">Transmettre des données</button>
+
+            </template>
+
+            <template #end>
+                <SelectButton v-model="value" :options="options" aria-labelledby="basic" />
+            </template>
+
+        </Toolbar>
+
         <Panel :selectedScenario="this.selectedScenario" :selectedGraph="this.selectedGraph"></Panel>
 
-    </template>
-
-    <template #end>
-      <SelectButton v-model="value" :options="options" aria-labelledby="basic" />
-    </template>
-
-  </Toolbar>
-  </div>
+    </div>
 </template>
 
 
@@ -54,11 +57,11 @@ export default {
     },
 
     data() {
-        return {                    
-            value : ref('Gâvres'),
-            options : ref(['Gâvres', 'Arcachon']),
-            checked : ref(false),
-    
+        return {
+            value: ref('Gâvres'),
+            options: ref(['Gâvres', 'Arcachon']),
+            checked: ref(false),
+
             scenario: null,
             selectedScenario: [],
             selectedGraph: [],
@@ -68,16 +71,16 @@ export default {
                     name: '2D',
                     code: '2D',
                     states: [
-                        { name: 'Graph2D 1'},
-                        { name: 'Graph2D 2'},
-                        { name: 'Graph2D 3'}]
+                        { name: 'Graph2D 1' },
+                        { name: 'Graph2D 2' },
+                        { name: 'Graph2D 3' }]
                 },
                 {
                     name: '3D',
                     code: '3D',
                     states: [
-                        {name: 'Graph3D 1'},
-                        {name: 'Graph3D 2'}]
+                        { name: 'Graph3D 1' },
+                        { name: 'Graph3D 2' }]
                 },
             ]
         }
@@ -91,34 +94,34 @@ export default {
         //  Récupérer le nom des dossiers et les transmettre à la variable scenario
         getDossier() {
             fetch('http://127.0.0.1:5000/arboresance')
-            .then(response => response.json())
-            .then(data => {
-                var L = [];
+                .then(response => response.json())
+                .then(data => {
+                    var L = [];
 
-                for (const property in data){
-                    var dict = {
-                        name: `${data[property]}`,
-                        code: `${data[property]}`
-                    };
-                    L.push(dict);
-                }
+                    for (const property in data) {
+                        var dict = {
+                            name: `${data[property]}`,
+                            code: `${data[property]}`
+                        };
+                        L.push(dict);
+                    }
 
-                this.scenario = L;
-            });
+                    this.scenario = L;
+                });
         },
-    }, 
+    },
 
 }
 </script>
 
 
 <style>
-.toolBar{
+.toolBar {
     z-index: 2;
     position: absolute;
-    width:100%;
-    height:5%;
-    top:0%;
+    width: 100%;
+    height: 5%;
+    top: 0%;
 }
 
 #app .p-toolbar {
@@ -129,12 +132,12 @@ export default {
     gap: 0.5rem;
 }
 
-.p-toolbar-group-center	{
-  padding: 0px;
+.p-toolbar-group-center {
+    padding: 0px;
 }
 
-.p-component{
-  padding: 0px;
+.p-component {
+    padding: 0px;
 }
 
 
