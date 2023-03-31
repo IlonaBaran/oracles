8052ceb22d6074fea99477b1cf6707e6b55e93bd
Jacqueline <jackie.rae.williams@gmail.com>
2023-03-31 14:06:06 +0200

    bati_flux ok

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -15,6 +15,10 @@ import {
   demLayer, demHRLayer, courbeNiveauLayer
 } from '../services/WMTS_service.js'
 
+import {
+  bati3DLayer
+} from '../services/WFS_service.js'
+
 export default {
   name: 'mapComponent',
   components: {
@@ -60,55 +64,10 @@ export default {
     // view.addLayer(routeLayer);
     // view.addLayer(batiLayer);
     // view.addLayer(courbeNiveauLayer);
+    view.addLayer(bati3DLayer);
 
     // view.removeLayer(planIGNv2Layer.id);
 
-
-
-    const batsource = new FileSource({
-      url: 'gavres_bati.geojson',
-      crs: 'EPSG:2154',
-      format: 'application/json',
-    });
-
-    let basic = new FeatureGeometryLayer('basic', {
-      // Use a FileSource to load a single file once
-      source: batsource,
-      transparent: true,
-      opacity: 0.7,
-      //zoom: { min: 10 },
-      style: new Style({
-        fill: {
-          color: setColor,
-          base_altitude: setAltitude,
-          extrusion_height: setExtrusion,
-        }
-      })
-    });
-
-    view.addLayer(basic);
-
-    function setAltitude(properties) {
-      if (properties.altitude_sol != null) {
-        return properties.altitude_sol + properties.hauteur;
-      } else {
-        //What to do when there is not floor value?
-        return 5;
-      }
-    }
-
-    function setExtrusion(properties) {
-      return properties.hauteur;
-    }
-
-    function setColor(properties) {
-
-      return new THREE.Color(0xffaaaa);
-    }
-
-
-  },
-  methods: {
   }
 }
 </script>
