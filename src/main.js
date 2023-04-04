0656d9320da06baf8f688f6947505f864caeaa68
lfredoc <leafredoc@gmail.com>
2023-04-04 15:08:18 +0200

    change theme

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import PrimeVue from 'primevue/config';
 import StyleClass from 'primevue/styleclass';
 
 // Import des librairies css pour primevue
-import "primevue/resources/themes/lara-light-indigo/theme.css";  //theme   
+import "primevue/resources/themes/lara-light-teal/theme.css";  //theme   
 import "primevue/resources/primevue.min.css";  //core
 import "primeicons/primeicons.css"; //icons
 
