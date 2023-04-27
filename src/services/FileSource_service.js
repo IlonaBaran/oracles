a5ba9d84b3c1b5a4851e820c6243edce7397c126
lfredoc <leafredoc@gmail.com>
2023-04-27 11:50:09 +0200

    trad premier service

--- src/services/FileSource_service.js
+++ src/services/FileSource_service.js
@@ -2,61 +2,61 @@
 import { FileSource, THREE, Style, FeatureGeometryLayer } from "../../node_modules/itowns/dist/itowns";
 
 /**
- * Function that extrudes buildings from building height data
- * @param {Object} properties properties object from building layer
+ * Fonction permettant d'extruder les bâtiments à partir des données relatives à leur hauteur
+ * @param {Object} properties objet de propriété de la couche bâtiment
  */
 function setAltitude(properties) {
-    //if there is altitude_sol property in layer && it's not null use it
+    //si la propriété altitude_sol existe dans la couche et qu'elle n'est pas nulle, elle est utilisée
     if (properties.altitude_sol != null) {
         return properties.altitude_sol;
     } else {
-        //else return a fixed value, 0 is good for near ocean
+        //sinon renvoie une valeur fixe, 0 est bon pour l'océan proche
         return 0;
     }
 }
 
 /**
- * Function that sets building ground elevation from building height data
- * @param {Object} properties properties object from building layer
+ * Fonction qui définit l'élévation du sol du bâtiment à partir des données relatives à la hauteur du bâtiment
+ * @param {Object} properties objet de propriété de la couche bâtiment
  */
 function setExtrusion(properties) {
-    //return the building height
+    //retourner la hauteur du bâtiment
     return properties.hauteur;
 }
 
 /**
- * Function that set color of buildings to grey
- * @param {Object} properties properties object from building layer
+ * Fonction qui fixe la couleur des bâtiments en gris
+ * @param {Object} properties objet de propriété de la couche bâtiment
  */
 function setColor(properties) {
-    //set the color to gray
+    //définir la couleur en gris
     return new THREE.Color(0xaaaaaa);
 }
 
 /**
- * Building FileSource
+ * création de FileSource
  */
 const batSource = new FileSource({
-    //retrieve building source from geojson file
+    //récupération des bâtiments à partir d'un fichier geojson
     url: 'http://localhost:8080/gavres_bati.geojson',
     crs: 'EPSG:2154',
     format: 'application/json',
 });
 
 /**
- * Building FeatureGeometryLayer
+ * Construction de la couche FeatureGeometryLayer
  */
-//create new FeatureGeometryLayer from building source
+//création d'un nouveau FeatureGeometryLayer à partir d'une source de construction
 export let bati = new FeatureGeometryLayer('bati', {
-    // Use a FileSource to load a single file once
+    // Utiliser un FileSource pour charger un seul fichier en une seule fois
     source: batSource,
-    //set style
+    //Création du style
     transparent: true,
     opacity: 0.7,
     //zoom: { min: 10 },
     style: new Style({
         fill: {
-            //use the extrusion functions
+            //utilisation des fonctions d'extrusion
             color: setColor,
             base_altitude: setAltitude,
             extrusion_height: setExtrusion,
